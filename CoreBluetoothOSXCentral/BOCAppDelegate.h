ec2cfc05b111af4bf00b50d6057144f781e2b259
Michael Reneer <michaelreneer@gmail.com>
2013-04-09 04:23:53 -0700

    Initial commit.

---
+++ CoreBluetoothOSXCentral/BOCAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  BOCAppDelegate.h
+//  CoreBluetoothOSXCentral
+//
+//  Created by Michael Reneer on 4/8/13.
+//  Copyright (c) 2013 Michael Reneer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <IOBluetooth/IOBluetooth.h>
+
+#pragma mark - Interface
+
+@interface BOCAppDelegate : NSObject <NSApplicationDelegate, CBCentralManagerDelegate, CBPeripheralDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CoreBluetoothOSXCentral/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CoreBluetoothOSXCentral
+//
+//  Created by Michael Reneer on 4/9/13.
+//  Copyright (c) 2013 Michael Reneer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
